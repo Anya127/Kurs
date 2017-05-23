abb14805751e905f3a4ff2950977abe315f7a253
Anya127 <anya_anya127@mail.ru>
2017-05-24 00:22:29 +0300

    третий блок изменения

--- app/models/film.rb
+++ app/models/film.rb
@@ -3,7 +3,7 @@ class Film < ActiveRecord::Base
   has_many :addresses
 
   before_destroy :destroy_addresses_parts
-  
+
   accepts_nested_attributes_for :parts, allow_destroy: true
 
   validates :name_f, presence: true
@@ -13,17 +13,17 @@ class Film < ActiveRecord::Base
   validates :companies_f, presence: true
   validates :min_age, presence: true
 
-  
+
 
 
   def destroy_addresses_parts
     self.addresses.each{|x| x.destroy}
     self.parts.each{|x| x.destroy}
   end
-  
+
   def self.search(params)
-    result = Film
-    
+    result = Film.eager_load(:parts, parts: :actor)
+
     if params['min_age'].present?
       result = result.where(min_age: params['min_age'])
     end
@@ -41,6 +41,23 @@ class Film < ActiveRecord::Base
     end
     if params['name_f'].present?
       result = result.where(name_f: params['name_f'])
+    end
+
+    if params['role_a'].present?
+      result = result.where("parts.role_a": params['role_a'])
+    end
+
+    if params['f_name'].present?
+      result = result.where("actors.f_name": params['f_name'])
+    end
+    if params['l_name'].present?
+      result = result.where("actors.l_name": params['l_name'])
+    end
+    if params['s_name'].present?
+      result = result.where("actors.s_name": params['s_name'])
+    end
+    if params['d_birthday'].present?
+      result = result.where("actors.d_birthday": params['d_birthday'])
     end
     result.all
   end
