665e4d2672f8b8ad9c01ebed639ad5a1b4b33054
Anya127 <anya_anya127@mail.ru>
2017-05-28 15:08:26 +0300

    последние исправления блока 2 и 3

--- app/models/part.rb
+++ app/models/part.rb
@@ -7,7 +7,7 @@ class Part < ActiveRecord::Base
 
   # В фильме нет повторяющейся роли с тем же атёром
   def uniq_role_actor_in_film
-    if self.film.parts.where(role_a: role_a).map{|x| x.actor.id}.include?(actor_id)
+    if self.film && self.film.parts.where(role_a: role_a).map{|x| x.actor.id}.include?(actor_id)
       errors.add(:role_a, "Роль с таким актёром уже существует")
     end
   end
